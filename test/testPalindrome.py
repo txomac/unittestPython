6276b6afcae95c594e66a5c81060aded7b74bc37
txomac <thomas.hnf@gmail.com>
2023-01-25 21:24:57 +0100

    add tests for Language in testPalindrome and testSalutation

--- test/testPalindrome.py
+++ test/testPalindrome.py
@@ -1,26 +1,32 @@
 import unittest
 
-from src.Ohce import Ohce
+from OhceBuilder import OhceBuilder
+from parameterized import parameterized
 
 
 class PalindromeTest(unittest.TestCase):
     def test_renvoi_miroir(self):
         chaine = "toto"
 
-        ohce = Ohce()
+        ohce = OhceBuilder.default()
         chaine_renvoye = ohce.revert(chaine)
 
         self.assertIn(chaine[::-1], chaine_renvoye)
 
-    def test_palindrome(self):
-        chaine = "radar"
+    @parameterized.expand([
+        [LangueAnglaise(), "Well done"],
+        [LangueFr(), "Bien dit"],
+    ])
+    def test_palindrome(self, languechoisie, bien_dit):
+        palindrome = "radar"
 
-        ohce = Ohce()
-        chaine_renvoye = ohce.revert(chaine)
-        self.assertIn(chaine_renvoye, chaine)
+        ohce = OhceBuilder().langue(languechoisie).build()
 
-        resultat_apres_palindrome = ohce.palindrome(chaine)
-        self.assertIn(chaine + "Bien dit", resultat_apres_palindrome)
+        resultat = ohce.palindrome(palindrome)
+        self.assertIn(palindrome, resultat)
+
+        resultat_apres_palindrome = resultat[len(palindrome):len(resultat)]
+        self.assertIn(palindrome + bien_dit, resultat_apres_palindrome)
 
 
 if __name__ == '__main__':
--- test/testSalutation.py
+++ test/testSalutation.py
@@ -5,18 +5,24 @@ from src.Ohce import Ohce
 
 class SalutationTest(unittest.TestCase):
 
-    def test_bonjour(self):
-        chaine = "Bonjour"
-
-        ohce = Ohce()
+    @parameterized.expand(
+        [
+            [LangueAnglaise(), "Hello"],
+            [LangueFr(), "Bonjour"],
+        ])
+    def test_bonjour(self, languechoisie, attendu):
+        ohce = OhceBuilder().langue(languechoisie).build()
         resultat = ohce.palindrome("test")
 
-        self.assertEqual(chaine, resultat[0:len(chaine)])
+        self.assertEqual(attendu, resultat[0:len(attendu)])
 
-    def test_au_revoir(self):
-        chaine = "Au revoir"
-
-        ohce = Ohce()
+    @parameterized.expand(
+        [
+            [LangueAnglaise(), "Goodbye"],
+            [LangueFr(), "Au revoir"],
+        ])
+    def test_au_revoir(self, languechoisie, attendu):
+        ohce = OhceBuilder().langue(languechoisie).build()
         resultat = ohce.palindrome("test")
 
-        self.assertEqual(chaine, resultat[-len(chaine):])
+        self.assertEqual(attendu, resultat[-len(attendu):])
