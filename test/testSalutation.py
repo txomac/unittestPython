c295cc396525d80bd8fc1d335d1540d66f5b4bc4
txomac <thomas.hnf@gmail.com>
2023-01-31 20:56:20 +0100

    étape 3 : add test with PeriodeJournée

--- test/testSalutation.py
+++ test/testSalutation.py
@@ -3,6 +3,7 @@ import unittest
 from parameterized import parameterized
 
 from OhceBuilder import OhceBuilder
+from src.PeriodeJournee import PeriodeDeLaJournee
 from src.langue.Constantes import Constantes
 from src.langue.LangueAng import LangueAnglaise
 from src.langue.LangueFr import LangueFr
@@ -12,22 +13,30 @@ class SalutationTest(unittest.TestCase):
 
     @parameterized.expand(
         [
-            [LangueAnglaise(), Constantes.Anglais.HELLO],
-            [LangueFr(), Constantes.Francais.BONJOUR],
+            [LangueAnglaise(), PeriodeDeLaJournee.DEFAULT, Constantes.Anglais.HELLO],
+            [LangueAnglaise(), PeriodeDeLaJournee.MATIN, Constantes.Anglais.GOOD_MORNING],
+            [LangueAnglaise(), PeriodeDeLaJournee.APRES_MIDI, Constantes.Anglais.GOOD_AFTERNOON],
+            [LangueAnglaise(), PeriodeDeLaJournee.SOIR, Constantes.Anglais.GOOD_EVENING],
+            [LangueAnglaise(), PeriodeDeLaJournee.NUIT, Constantes.Anglais.GOOD_NIGHT],
+            [LangueFr(), PeriodeDeLaJournee.DEFAULT, Constantes.Francais.BONJOUR],
+            [LangueFr(), PeriodeDeLaJournee.MATIN, Constantes.Francais.BONJOUR],
+            [LangueFr(), PeriodeDeLaJournee.APRES_MIDI, Constantes.Francais.BONJOUR],
+            [LangueFr(), PeriodeDeLaJournee.SOIR, Constantes.Francais.BONSOIR],
+            [LangueFr(), PeriodeDeLaJournee.NUIT, Constantes.Francais.BONSOIR],
         ])
-    def test_bonjour(self, languechoisie, attendu):
-        ohce = OhceBuilder().langue(languechoisie).build()
+    def test_bonjour(self, languechoisie, periodechoisie, attendu):
+        ohce = OhceBuilder().langue(languechoisie).periode(periodechoisie).build()
         resultat = ohce.palindrome("test")
 
         self.assertEqual(attendu, resultat[0:len(attendu)])
 
     @parameterized.expand(
         [
-            [LangueAnglaise(), Constantes.Anglais.GOOD_BYE],
-            [LangueFr(), Constantes.Francais.AU_REVOIR],
+            [LangueAnglaise(), PeriodeDeLaJournee.DEFAULT, Constantes.Anglais.GOOD_BYE],
+            [LangueFr(), PeriodeDeLaJournee.DEFAULT, Constantes.Francais.AU_REVOIR],
         ])
-    def test_au_revoir(self, languechoisie, attendu):
-        ohce = OhceBuilder().langue(languechoisie).build()
+    def test_au_revoir(self, languechoisie, periodechoisie, attendu):
+        ohce = OhceBuilder().langue(languechoisie).periode(periodechoisie).build()
         resultat = ohce.palindrome("test")
 
         self.assertEqual(attendu, resultat[-len(attendu):])
